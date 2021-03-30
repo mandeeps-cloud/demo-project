3e03fa68a244ccb7b31949003f5e17d8648fd443
mandeeps-cloud <69319531+mandeeps-cloud@users.noreply.github.com>
2021-03-30 23:40:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "ap-south-1b"
   access_key = ""
   secret_key = ""
 }
@@ -15,7 +15,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "tf-example"
